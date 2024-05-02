56d38d8820bfb6d6f9e977e07dc00e9c927ece9e
jumashafara <jumashafara0@gmail.com>
2024-05-02 16:57:36 +0300

    setup files

---
+++ nbs/Python/my_module.py
@@ -0,0 +1,10 @@
+# this is my_module.py
+
+first_name = 'Viola'
+last_name = 'Akullu'
+
+def add(number1, number2):
+    return number1 + number2
+
+def multiply(number1, number2):
+    return number1 * number2
